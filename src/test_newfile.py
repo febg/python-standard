2486c147a9ace42e75294bc19a866bd41a85661d
Felipe <felipe@codecov.io>
2019-12-10 18:56:56 -0800

    Added test for newfile

---
+++ src/test_newfile.py
@@ -0,0 +1,12 @@
+import pytest
+import newfile
+
+def test_uncovered_if():
+    assert newfile.uncovered_if() == False
+
+# def test_fully_covered():
+#     assert index.fully_covered() == True
+
+
+
+
