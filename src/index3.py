c5ac5b079adafd2df600317a7d75052b1d8e834e
Felipe Ballesteros <felipe@codecov.io>
2020-02-18 09:41:46 -0500

    index 3

---
+++ src/index3.py
@@ -0,0 +1,12 @@
+def uncovered_if3(var=True):
+    if var:
+      return False
+    else:
+      return True
+
+def fully_covered3():
+    return True
+
+def uncovered3():
+    return True
+
---
+++ src/test_index3.py
@@ -0,0 +1,12 @@
+import pytest
+import index3
+
+def test_uncovered_if3():
+    assert index3.uncovered_if3() == False
+
+def test_fully_covered2():
+    assert index3.fully_covered3() == True
+
+
+
+
